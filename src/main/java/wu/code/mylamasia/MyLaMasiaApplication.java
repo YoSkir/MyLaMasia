38f017f5662f00fcb7bbea3abe5242386df5f96c
YoSkir <137788381+YoSkir@users.noreply.github.com>
2025-06-23 07:13:15 +0800

    -create

---
+++ src/main/java/wu/code/mylamasia/MyLaMasiaApplication.java
@@ -0,0 +1,13 @@
+package wu.code.mylamasia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLaMasiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyLaMasiaApplication.class, args);
+    }
+
+}
